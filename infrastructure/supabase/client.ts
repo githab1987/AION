2d2a093408e40f38990d52dcee39720dfcae06b7
githab1987 <githab1987@gmail.com>
2026-09-11 16:08:43 +0700

    Update client.ts

--- infrastructure/supabase/client.ts
+++ infrastructure/supabase/client.ts
@@ -17,6 +17,7 @@ if (!supabaseServiceRoleKey) {
 export const supabaseAdmin = createClient(
   supabaseUrl,
   supabaseServiceRoleKey,
+  supabaseAnonKey
   {
     auth: {
       autoRefreshToken: false,
